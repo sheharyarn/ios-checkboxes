fd77272a83478dfc149cf360051c90d197e9e19a
Sheharyar Naseer <sheharyar92@live.com>
2014-06-09 04:56:50 +0500

    radio select checkboxes

--- ioscheckboxes/staticCheckboxes.h
+++ ioscheckboxes/staticCheckboxes.h
@@ -9,9 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface staticCheckboxes : UIViewController
-@property (weak, nonatomic) IBOutlet UIButton *checkbox1;
-@property (weak, nonatomic) IBOutlet UIButton *checkbox2;
 @property (weak, nonatomic) IBOutlet UILabel *label1;
 @property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *labelmulti;
+
+@property (weak, nonatomic) IBOutlet UIButton *checkbox1;
+@property (weak, nonatomic) IBOutlet UIButton *checkbox2;
+
+@property (weak, nonatomic) IBOutlet UIButton *multi1_1;
+@property (weak, nonatomic) IBOutlet UIButton *multi1_2;
+@property (weak, nonatomic) IBOutlet UIButton *multi1_3;
 
 @end
--- ioscheckboxes/staticCheckboxes.m
+++ ioscheckboxes/staticCheckboxes.m
@@ -12,20 +12,37 @@
 checkboxButton *checkbox1;
 checkboxButton *checkbox2;
 
+checkboxButton *multi11;
+checkboxButton *multi12;
+checkboxButton *multi13;
+
 @interface staticCheckboxes ()
 @end
 
 @implementation staticCheckboxes
 @synthesize label1;
 @synthesize label2;
+@synthesize labelmulti;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // First Two Checkboxes
     checkbox1 = (checkboxButton *) _checkbox1;
     checkbox2 = (checkboxButton *) _checkbox2;
     
     [checkbox1 initWithCheckedImage:@"checkbox_true" uncheckedImage:@"checkedbox_false"];
     [checkbox2 initWithCheckedImage:@"checkbox_true" uncheckedImage:@"checkedbox_false"];
+    
+    
+    // Multiple Choice Checkboxes
+    multi11   = (checkboxButton *) _multi1_1;
+    multi12   = (checkboxButton *) _multi1_2;
+    multi13   = (checkboxButton *) _multi1_3;
+    
+    [multi11 initWithCheckedImage:@"checkbox_true" uncheckedImage:@"checkedbox_false"];
+    [multi12 initWithCheckedImage:@"checkbox_true" uncheckedImage:@"checkedbox_false"];
+    [multi13 initWithCheckedImage:@"checkbox_true" uncheckedImage:@"checkedbox_false"];
 }
 
 - (IBAction)checkbox1Pressed:(id)sender {
@@ -40,6 +57,24 @@ checkboxButton *checkbox2;
         label2.text = @"Checked";
     else
         label2.text = @"Unchecked";
+}
+
+// Multiple Choice
+- (IBAction)m11pressed:(id)sender {
+    labelmulti.text = @"Option 1 Selected";
+    [multi12 setChecked:false];
+    [multi13 setChecked:false];
+}
+
+- (IBAction)m12pressed:(id)sender {
+    labelmulti.text = @"Option 2 Selected";
+    [multi11 setChecked:false];
+    [multi13 setChecked:false];
+}
+- (IBAction)m13pressed:(id)sender {
+    labelmulti.text = @"Option 3 Selected";
+    [multi11 setChecked:false];
+    [multi12 setChecked:false];
 }
 
 - (void)didReceiveMemoryWarning {
